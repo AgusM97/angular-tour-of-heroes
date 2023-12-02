65208b3cb904e1669b84878e89c10ea6fccb6053
AgusM97 <agus91997@gmail.com>
2023-12-02 01:52:34 -0300

    add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroesComponent, MessagesComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeroesComponent,
+    MessagesComponent,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,31 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { Hero } from '../hero';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [NgFor, HeroDetailComponent],
+  imports: [NgFor, RouterLink],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
   constructor(
     private heroService: HeroService,
     public messageService: MessageService
   ) {}
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(this.selectedHero.name + 'selected');
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
